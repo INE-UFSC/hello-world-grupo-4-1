4c4c5cad94117330aa81ef882010ee0742909aeb
JPLdeCamargo <93204586+JPLdeCamargo@users.noreply.github.com>
2021-10-26 11:23:40 -0300

    Update membro2.py

--- membro2.py
+++ membro2.py
@@ -1,2 +1,5 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return '''Olá, meu nome é João Pedro, 18 anos.
+Minha experiência de programação é basicamente POO1, porém agora também estou estudando front end e estrutura de dados no meu tempo livre.
+Gosto de jogar e programar uns projetinhos inuteis.
+Eu espero ser um programador melhor em geral até o final dessa matéria.''' #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
