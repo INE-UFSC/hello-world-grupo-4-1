902f3a3ea6a9971f146bef65821a45c6fbde5add
João Vitor Santos <82539207+Coala1za1@users.noreply.github.com>
2021-10-26 11:25:24 -0300

    membro1.py

--- membro1.py
+++ membro1.py
@@ -1,2 +1,4 @@
 def hello():
-	return "Olá, eu me chamo João Vitor dos Santos, tenho 20 anos, sou de Laguna/SC. Costumo como passatempo jogar alguns jogos de FPS e um lolzinho as vezes, gosto de andar de moto, a respeito de programação a única experiencia que tenho é de POO I(tenham paciência comigo) e nesse curso espero encontrar algo que me de vontade de seguir no ramo pelo resto da minha vida"
+	return '''Olá, eu me chamo João Vitor dos Santos, tenho 20 anos, sou de Laguna/SC. 
+	Costumo como passatempo jogar alguns jogos de FPS e um lolzinho as vezes, gosto de andar de moto.
+	A respeito de programação a única experiencia que tenho é de POO I(tenham paciência comigo) e nesse curso espero encontrar algo que me de vontade de seguir no ramo pelo resto da minha vida"
